51b2356b51492a44194f58f22d571d52a06e8430
pfrackowiakszymansk-clgx <pfrackowiakszymansk@corelogic.com>
2024-08-06 11:50:15 +0200

    Improve examples

--- src/main/java/type_erasure/StringParser.java
+++ src/main/java/type_erasure/StringParser.java
@@ -1,5 +1,13 @@
 package type_erasure;
-import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+//How to parse a string into an object?
+//1. create an "empty" instance
+//2. read fields from string
+//3. set fields on the instance
 
 public class StringParser {
   public static <T> T read(String rawData) {
@@ -16,20 +24,25 @@ public class StringParser {
     }
   }
 
-//  public static <T> T readProper(String rawData, Class<T> type) {
-//    try {
-//      T instance = type.getDeclaredConstructor()
-//          .newInstance();
-//      rawData.lines()
-//          .map(line -> line.split(",", 2))
-//          .forEach(parts -> setField(instance, parts[0], parts[1]));
-//
-//      return instance;
-//    } catch (ReflectiveOperationException e) {
-//      System.out.println(":-(");
-//      throw new RuntimeException(e);
-//    }
-//  }
+  public static <T> T readProper(String rawData, Class<T> type) {
+    try {
+      T instance = type.getDeclaredConstructor()
+          .newInstance();
+      rawData.lines()
+          .map(line -> line.split(",", 2))
+          .forEach(parts -> setField(instance, parts[0], parts[1]));
+
+      return instance;
+    } catch (ReflectiveOperationException e) {
+      System.out.println(":-(");
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static <T> T readByTypeReference(String rawData, TypeReference<T> typeReference) throws JsonProcessingException {
+    ObjectMapper mapper = new ObjectMapper();
+    return mapper.readValue(rawData, typeReference);
+  }
 
   private static <T> void setField(T instance, String field, String value) {
     var setter = "set" + field.substring(0, 1)
@@ -46,8 +59,9 @@ public class StringParser {
   public static void main(String[] args) {
     var rawData = "name,John Doe\ncountry,USA";
 
-    var person = StringParser.read(rawData);
+    Person person = StringParser.read(rawData);
 //    var actualPerson = StringParser.readProper(rawData, Person.class);
+//    Person alsoActualPerson = StringParser.readByTypeReference(rawData, new TypeReference<>() {});
 
     System.out.println(person);
   }
